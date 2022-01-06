e3a90f0dc6d82a40ede4ccc9fb5631db982ae694
eric felts <echristianfelts@gmail.com>
2022-01-05 16:58:09 -0800

    New Assets.  Finished Zenva Intro to Unity.

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour
+{
+    public int clicksToPop = 20;
+
+    void OnMouseDown()
+    {
+        clicksToPop -= 1;
+        transform.localScale += new Vector3(0.15f, 0.15f, 0.15f);
+
+        if(clicksToPop == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -7,7 +7,17 @@ public class FirstScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int score = 0;
+        string playerName = "Ted";
+        float jumpForce = 5.82f;
+        bool gameOver = false;
+
+        Debug.Log(transform.position);
+
+        Vector3 testVector = new Vector3(10, 10, 10);
+
+        transform.position += testVector;
+
     }
 
     // Update is called once per frame
