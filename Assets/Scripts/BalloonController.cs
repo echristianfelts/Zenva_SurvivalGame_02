673e3d4f09322d243105ffc5f654f9a2200dd2ee
eric felts <echristianfelts@gmail.com>
2022-01-07 14:35:52 -0800

    Zenva minigame Projects- 7/17.  Balloon popper thing.  UI stuff.

--- Assets/Scripts/BalloonController.cs
+++ Assets/Scripts/BalloonController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BalloonController : MonoBehaviour
 {
+    public int scoretoGive = 1;
     public int clicksToPop = 20;
+    public float scaleIncreasePerClick = 0.1f;
+    public ScoreManager scoreManager;
 
     void OnMouseDown()
     {
         clicksToPop -= 1;
-        transform.localScale += new Vector3(0.15f, 0.15f, 0.15f);
-
-        if(clicksToPop == 0)
+        //  transform.localScale += new Vector3(0.15f, 0.15f, 0.15f);
+        transform.localScale += Vector3.one * scaleIncreasePerClick;
+        if (clicksToPop == 0)
         {
+            scoreManager.IncreaseScore(scoretoGive);
             Destroy(gameObject);
         }
     }
